5f6b34b7c43c1a750a1dd2a489bd9eadc5259202
Tiberius <35663392+TIberiu99p@users.noreply.github.com>
2019-03-04 16:39:59 +0000

    Updated main.py
    i added the menu as it follows the instructions of the assignment paper

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello world")
+def main():
+    print("Select the option you wish to perform: \nA)Add student \nB)Remov student \nL)List Database \nX)Exit")
+    choices = ['A','B','L','X']   
+    choice = input()
+    if choice == 'A':
+        print("choice a works")
+    elif choice == 'B':
+        print("choice b works")
+    elif choice == 'L':
+        print("choice l works")
+    elif choice == 'X':
+        print("choice x works")
+    else:
+        if choice not in choices:
+            print("Select a valid option")
+            main()
+            
+
+
+main()
